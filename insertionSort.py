cf18c42285affb1dc25f47944a3c01ca57d6ba3e
Manan Joshi <joshimanan265@gmail.com>
2024-06-29 12:38:40 +0530

    Add files via upload

---
+++ insertionSort.py
@@ -0,0 +1,12 @@
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    return lst
+
+numbers = [12, 11, 13, 5, 6]
+print(insertion_sort(numbers))
